c6d4548ac66bd0bf765975e9dc6be8569dc1bac5
adnapp <adam.napps@gmail.com>
2021-01-19 16:50:33 -0500

    commit tree 8f9677246b834b03aff37367d12d71fe85e22a19

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [allListings, setAllListings] = useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(() => {
+    fetch(`http://localhost:6001/listings`)
+    .then(response => response.json())
+    .then(data => setAllListings(data))
+  },[])
+
+  function onDelete(delObj){
+    console.log(delObj)
+
+    fetch(`http://localhost:6001/listings/${delObj.id}`, {
+      method: 'DELETE'    })
+    .then(response => response.json())
+    .then((data) => {
+      const postDelete = allListings.filter(listing => {
+        return listing.id !== delObj.id
+      })
+      setAllListings(postDelete)
+    })}
+
+    function handleSearch(incomingSearch){
+      setSearch(incomingSearch)
+    }
+
+
+  const searchedObjects = allListings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  } )
+
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header handleSearch={handleSearch}/>
+      <ListingsContainer allListings={searchedObjects} onDelete={onDelete}/>
     </div>
   );
 }
